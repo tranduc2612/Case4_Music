5122bbc44d4cfe7c5a73e9e1eb2f7655da30afcb
Duc <mintduc2612@gmail.com>
2023-04-17 23:21:05 +0700

    FE

--- src/routes/album-router.ts
+++
@@ -1,8 +0,0 @@
-import {Router} from "express";
-import albumController from "../app/controllers/albumController";
-
-const albumRouter = Router();
-albumRouter.post('/create',albumController.create);
-albumRouter.get('/get',albumController.getAll);
-
-export default albumRouter
